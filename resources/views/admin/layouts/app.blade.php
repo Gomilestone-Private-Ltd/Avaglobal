a933198b6de1a718158c545585fa8bb7f0b90e79
Kanhaiya2580 <Kanhaiya@gomilestone.com>
2024-03-23 18:02:49 +0530

    admin dashborad

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html class="no-js " lang="en">
+@include('admin.layouts.header')
+
+<body class="theme-blush">
+    @yield('content');
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,3 +27,7 @@ Route::get('career', [HomeController::class, 'career'])->name('career');
 Route::get('case-study', [HomeController::class, 'caseStudy'])->name('caseStudy');
 Route::get('career-detail', [HomeController::class, 'careerDetails'])->name('careerDetails');
 Route::get('case-study-detail', [HomeController::class, 'caseStudyDetail'])->name('caseStudyDetail');
+
+
+//Admin panel
+Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
