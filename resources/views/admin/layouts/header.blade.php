454208664554959458f220f111c2119f72350aab
Kanhaiya <Kanhaiya@gomilestone.com>
2024-04-10 12:33:51 +0530

    changes

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -61,7 +61,7 @@
                                                 class="logout-icon"></span>Logout</a></li>
                             </ul>
                         </div>
-                        <small class="userrole">{{ auth()->user()->role->name }}</small>
+                        {{-- <small class="userrole">{{ auth()->user()->role->name }}</small> --}}
                     </div>
                 </div>
             </div>
