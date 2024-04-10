d34b9cb09149d67b87da8b4195f887bff6018572
Kanhaiya2580 <Kanhaiya@gomilestone.com>
2024-04-10 14:10:50 +0530

    chnages

--- resources/views/admin/layouts/loader.blade.php
+++ resources/views/admin/layouts/loader.blade.php
@@ -18,10 +18,5 @@
 </div> -->
 
 <!-- Right Icon menu Sidebar -->
-<!-- <div class="navbar-right">
-    <ul class="navbar-nav">
-
-        <li><a href="{{ route('logout') }}" class="mega-menu" title="Sign Out"><i class="zmdi zmdi-power"></i></a>
-        </li>
-    </ul>
+<div class="navbar-right">
 </div>
